5ee9bc8346cf986e2d1b8e497317ae01ea149497
BJOSIM1 <bjosim1@pax.priv.no>
2026-02-25 12:57:56 +0100

    feat: setup lockfile

---
+++ env/.config/fish/functions/jwt-decode.fish
@@ -0,0 +1,3 @@
+function jwt-decode
+  echo $argv[1] | jq -R 'split(".") |.[0:2] | map(gsub("-"; "+") | gsub("_"; "/") | gsub("%3D"; "=") | @base64d) | map(fromjson)'
+end
--- env/.config/nvim/init.lua
+++ env/.config/nvim/init.lua
@@ -15,15 +15,15 @@ vim.opt.signcolumn = 'yes'
 vim.g.mapleader = ' '
 
 vim.pack.add({
-	{ src = 'https://github.com/ellisonleao/gruvbox.nvim' },
-	{ src = 'https://github.com/stevearc/oil.nvim' },
-	{ src = 'https://github.com/echasnovski/mini.pairs' },
-	{ src = 'https://github.com/echasnovski/mini.pick' },
-	{ src = 'https://github.com/mason-org/mason.nvim' },
-	{ src = 'https://github.com/neovim/nvim-lspconfig' },
-	{ src = 'https://github.com/kdheepak/lazygit.nvim' },
-	{ src = 'https://github.com/kylechui/nvim-surround' },
-	{ src = 'https://github.com/saghen/blink.cmp', version = '1.*' },
+	{ src = 'https://github.com/ellisonleao/gruvbox.nvim', version = '5e0a460d8e0f7f669c158dedd5f9ae2bcac31437' },
+	{ src = 'https://github.com/stevearc/oil.nvim', version = '01cb3a8ad7d5e8707041edc775af83dbf33838f4' },
+	{ src = 'https://github.com/echasnovski/mini.pairs', version = 'b316e68f2d242d5bd010deaab645daa27ed86297' },
+	{ src = 'https://github.com/echasnovski/mini.pick', version = 'b4351399db5043a781a32699a1bde60b747f711f' },
+	{ src = 'https://github.com/mason-org/mason.nvim', version = '57e5a8addb8c71fb063ee4acda466c7cf6ad2800' },
+	{ src = 'https://github.com/neovim/nvim-lspconfig', version = 'f1237a8b3c243608a7116fa1ad216c441d7c2264' },
+	{ src = 'https://github.com/kdheepak/lazygit.nvim', version = '2305deed25bc61b866d5d39189e9105a45cf1cfb' },
+	{ src = 'https://github.com/kylechui/nvim-surround', version = 'fcfa7e02323d57bfacc3a141f8a74498e1522064' },
+	{ src = 'https://github.com/saghen/blink.cmp', version = '4b18c32adef2898f95cdef6192cbd5796c1a332d' },
 })
 
 require 'mini.pick'.setup()
